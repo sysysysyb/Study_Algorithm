9b3dc923b4c3e5e43217020f7d1c23608d0a6fd4
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-04-24 18:49:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.42 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,13 +1,13 @@
 function solution(absolutes, signs) {
     
-    // let sum = 0;
-    // for (let i in signs) {
-    //     // sum += `${signs[i] ? '+' : '-'}${absolutes[i]}`*1; // #1
-    //     sum += absolutes[i] * (signs[i] ? 1 : (-1)); // #2
-    // }
+    let sum = 0;
+    for (let i in signs) {
+        // sum += `${signs[i] ? '+' : '-'}${absolutes[i]}`*1; // #1
+        // sum += absolutes[i] * (signs[i] ? 1 : (-1)); // #2
+    }
     
-    // absolutes.forEach((e,i) => sum += e * (signs[i] ? 1 : (-1))); // #3
-    // return sum;
+    absolutes.forEach((e,i) => sum += e * (signs[i] ? 1 : (-1))); // #3
+    return sum;
 
-    return (absolutes.reduce((acc, cur, i) => acc + cur * (signs[i] ? 1 : (-1)), 0)); // #4
+    // return (absolutes.reduce((acc, cur, i) => acc + cur * (signs[i] ? 1 : (-1)), 0)); // #4
 }
