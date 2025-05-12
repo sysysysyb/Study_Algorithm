0a391960f539186edc11efbdbb5eab056435cd3e
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-05-12 17:26:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,26 @@
+function solution(n) {
+    let num = 1;
+    let square = 0;
+    
+    while (true) {
+        num *= 3;
+        if (n < num) break;
+        square++;
+    };
+    
+    const list = Array(square+1).fill(0).map((e) => {
+        num = 0;
+        for (let i = 0; i < 2; i++) {
+            if ((n - 3**square) >= 0) {
+                n -= 3**square;
+                num++;
+            };
+        }
+        square--;
+        return num;
+    })
+    
+    return(list.reduceRight((acc, cur, i) => {
+        return acc + cur * 3 ** i;
+    }, 0));
+}
