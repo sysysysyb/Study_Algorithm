4111579600f5ecf271b28f3b214b0a751566f012
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-05-19 18:22:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.92 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,20 +1,33 @@
-function solution(numbers) {
-    // numbers 배열 오름차순 정렬
-    // 만약 현재 요소가 다다음 요소와 같은 숫자라면 pass
-    // 반복문으로 뒤의 요소들과의 합 계산 및 저장
+// function solution(numbers) {
+//     // numbers 배열 오름차순 정렬
+//     // 만약 현재 요소가 다다음 요소와 같은 숫자라면 pass
+//     // 반복문으로 뒤의 요소들과의 합 계산 및 저장
     
-    let sn = numbers.sort((a, b) => a - b);
-    let result = [];
+//     let sn = numbers.sort((a, b) => a - b);
+//     let result = [];
     
-    for (let i = 0; i < sn.length - 1; i++) {
-        if (sn[i] === sn[i + 2]) continue;
+//     for (let i = 0; i < sn.length - 1; i++) {
+//         if (sn[i] === sn[i + 2]) continue;
         
-        for (let j = i + 1; j < sn.length; j++) {
-            if (result.includes(sn[i] + sn[j]) === false) {
-                result.push(sn[i] + sn[j]);
-            }
+//         for (let j = i + 1; j < sn.length; j++) {
+//             if (result.includes(sn[i] + sn[j]) === false) {
+//                 result.push(sn[i] + sn[j]);
+//             }
+//         }
+//     }
+    
+//     return result.sort((a, b) => a - b);
+// }
+function solution(numbers) {
+    const temp = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            temp.push(numbers[i] + numbers[j])
         }
     }
-    
-    return result.sort((a, b) => a - b);
+
+    const answer = [...new Set(temp)]
+
+    return answer.sort((a, b) => a - b)
 }
