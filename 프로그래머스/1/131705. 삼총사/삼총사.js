b5836e6790d0e1ae0469a998e3a0ca174af45a4b
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-05-25 21:47:32 +0900

    [level 1] Title: 삼총사, Time: 0.58 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,17 +1,39 @@
 function solution(n) {
+//     let answer = 0;
+    
+//     const isThree = (x,y,z) => {
+//         return (n[x] + n[y] + n[z]) === 0 ? 1 : 0;
+//     };
+    
+//     for (let i = 0; i < (n.length); i++) {
+//         for (let j = i+1; j < (n.length); j++) {
+//             for (let k = j+1; k < (n.length); k++) {
+//                 answer = isThree(i,j,k) + answer;
+//             }
+//         }
+//     }
+    
+//     return answer;
+    
+    // 재귀함수로 구현하기
     let answer = 0;
     
-    const isThree = (x,y,z) => {
-        return (n[x] + n[y] + n[z]) === 0 ? 1 : 0;
-    };
-    
-    for (let i = 0; i < (n.length); i++) {
-        for (let j = i+1; j < (n.length); j++) {
-            for (let k = j+1; k < (n.length); k++) {
-                answer = isThree(i,j,k) + answer;
-            }
+    function isThree(idx, arr) {
+        // Base Case
+        if (arr.length === 3) {
+            const [first, second, third] = arr;
+            answer = (first + second + third === 0) ? answer + 1 : answer;
+            return;
         }
+        
+        // Recursive Case
+        for (let i = idx; i < n.length; i++) {
+            isThree(i + 1, [...arr, n[i]]);
+        }
+        
     }
+    
+    isThree(0, []);
     
     return answer;
 }
