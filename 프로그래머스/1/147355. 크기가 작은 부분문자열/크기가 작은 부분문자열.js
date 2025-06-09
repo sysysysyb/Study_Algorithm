550dc3dc8b2f66d63a693bd16f576829c603a219
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-06-09 21:01:05 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 31.78 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,16 +1,33 @@
 function solution(t, p) {
+    // 슬라이딩 윈도우 패턴
+    const pl = p.length;
+    let str = '';
+    let answer = 0;
     
-    const list = [];
-    for (let i = 0; i <= t.length - p.length; i++) {
-        list.push(Number(t.slice(i, i + p.length)));
-    };
-    
-    list.sort((a, b) => a - b);
-    
-    let num = 0;
-    while (list[num] <= Number(p)) {
-        num++;
+    for (let i = 0; i < t.length; i++) {
+        str += t[i];
+        
+        if (i >= pl - 1) {
+            answer += Number(str) <= Number(p) ? 1 : 0;
+            str = str.slice(1);
+        }
     }
     
-    return num;
+    return answer;
+    
+    
+    // 이전 풀이
+//     const list = [];
+//     for (let i = 0; i <= t.length - p.length; i++) {
+//         list.push(Number(t.slice(i, i + p.length)));
+//     };
+    
+//     list.sort((a, b) => a - b);
+    
+//     let num = 0;
+//     while (list[num] <= Number(p)) {
+//         num++;
+//     }
+    
+//     return num;
 }
