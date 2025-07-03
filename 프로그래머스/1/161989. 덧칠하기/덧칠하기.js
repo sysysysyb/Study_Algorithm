ea9a3c4d5c39cdddcd9da3d2cab57b9d9f725adb
seo_0 <87892147+sysysysyb@users.noreply.github.com>
2025-07-03 13:25:11 +0900

    [level 1] Title: 덧칠하기, Time: 5.42 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,16 +1,15 @@
 function solution(n, m, section) {
-    const l = section.length;
-    let p = 0;      // current section item
-    let c = 0;      // count
+    let cw = section[0];    // current wall
+    let nw = cw + m - 1;    // next wall
+    let count = 1;          // count
     
-    while (p < l && p !== -1) {
-        const cw = section[p];  // current wall
-        const nw = cw + m;      // next wall
-        
-        p = section.findIndex(el => el >= nw);
-        // console.log(`p : ${p}`);
-        c++;
+    for (const s of section) {
+        if (s > nw) {
+            cw = s;
+            nw = cw + m - 1;
+            count++;
+        }
     }
     
-    return c;
+    return count;
 }
