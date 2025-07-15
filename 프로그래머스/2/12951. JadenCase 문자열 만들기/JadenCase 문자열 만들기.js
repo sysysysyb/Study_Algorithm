c8f1b697a3ffe8f5dbd28c8276511c8fd0fc95bf
seoyoung <87892147+sysysysyb@users.noreply.github.com>
2025-07-15 17:50:33 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    const arr = s.split(' ');
+    
+    for (const [idx, str] of arr.entries()) {
+        if (str === '') continue;
+
+        // console.log(`str : ${str}`);
+        const firstStr = typeof str[0] === Number ? str[0] : str[0].toUpperCase();
+        
+        if (str.length === 1) {
+            arr[idx] = firstStr;
+        } else {
+            const restStr = str.substring(1).toLowerCase();
+            arr[idx] = firstStr.concat("", restStr);
+        }
+    }
+    
+    // console.log(arr);
+    
+    return arr.join(' ');
+}
