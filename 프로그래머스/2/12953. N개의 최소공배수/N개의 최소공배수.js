4868f2375d99afd4720219f7b74c89e471a07679
seoyoung <87892147+sysysysyb@users.noreply.github.com>
2025-11-09 23:58:48 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    // 유클리드 호제법
+    // gcd(최대공약수) : gcd(a, b) = gcd(b, a % b)
+    // lcm(최소공배수) : lcm(a, b) = a * b * gcd(a, b)
+    
+    let answer = arr[0];
+    const getGCD = (a, b) => b === 0 ? a : getGCD(b, a % b);
+    const getLCM = (a, b) => (a / getGCD(a, b)) * b;
+    
+    for (let i = 1; i < arr.length; i++) {
+        answer = getLCM(answer, arr[i]);
+    }
+    
+    return answer;
+}
