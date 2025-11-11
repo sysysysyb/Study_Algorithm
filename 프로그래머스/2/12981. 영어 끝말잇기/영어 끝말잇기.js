d876b71d32d9bf30cab2a2bd9f68ae1a90b2da45
seoyoung <87892147+sysysysyb@users.noreply.github.com>
2025-11-12 00:17:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 3.69 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,26 @@
+function solution(n, words) {
+    let word = null;
+    let idx = 1;
+    const wordSet = new Set();
+    wordSet.add(words[0]);
+    
+    for (let i = 1; i < words.length; i++) {
+        const prevWord = words[i - 1];
+        const nextWord = words[i];
+        
+        if (wordSet.has(nextWord) || prevWord[prevWord.length - 1] !== nextWord[0]) {
+            word = nextWord;
+            idx += i;
+            break;
+        }
+        
+        wordSet.add(nextWord);
+    }
+    
+    if (word) {
+        console.log(idx);
+        return [idx % n === 0 ? n : idx % n, Math.ceil(idx / n)]
+    } else {
+        return [0, 0];
+    }
+}
